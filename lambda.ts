75756144ebe8c1a3671b9af62ff7bc1185ea66a3
bharathananth309 <bharathananth309@gmail.com>
2025-04-30 18:25:55 +0530

    Create lambda.ts

---
+++ lambda.ts
@@ -0,0 +1,18 @@
+// lambda.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.init();
+}
+
+bootstrap();
+
+export default server;
